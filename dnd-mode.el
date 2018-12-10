515bbe1fd2e36ac4026d433966dbded09a3e7916
biff <jeffreydbyrd@gmail.com>
2018-12-10 01:45:08 -0500

    added logic for negative bonus rolls

--- dnd-mode.el
+++ dnd-mode.el
@@ -33,8 +33,7 @@ buttons that will print the result of the dice roll depicted."
 	 (add-to-list '*dnd-mode-buttons*
 		     (make-button b-beg b-end
 				  'action (lambda (b) (print (dnd-roll b-n b-d b-b))))))
-	(setq b-data (dnd--find-next-button-data))
-	))))
+	(setq b-data (dnd--find-next-button-data))))))
 
 (defun dnd-unhighlight-dice-buttons ()
   (interactive)
@@ -86,18 +85,22 @@ buttons that will print the result of the dice roll depicted."
 
 ;; regexps for searching
 (setq dnd--regexp-1d2+3 "\\([0-9]+\\)d\\([0-9]+\\)\\+\\([0-9]+\\)") ;; 2d4+3
+(setq dnd--regexp-1d2-3 "\\([0-9]+\\)d\\([0-9]+\\)-\\([0-9]+\\)")   ;; 2d4-3
 (setq dnd--regexp-1d2 "\\([0-9]+\\)d\\([0-9]+\\)")                  ;; 2d4
 (setq dnd--regexp-d2 "d\\([0-9]+\\)")                               ;; d4
 (setq dnd--regexp-+3 "\\+\\([0-9]+\\)")                             ;; +3
+(setq dnd--regexp--3 "-\\([0-9]+\\)")                               ;; -3
 (setq dnd--regexp-33% "\\([0-9]+\\)%")                              ;; 33%
 (setq dnd--regexp-33-percent "\\([0-9]+\\) percent")                ;; 33 percent
 
 (setq dnd--dice-regexp
       (mapconcat (lambda (rx) (concat "[[:space:]\n(,]" rx "[[:space:]\n),]"))
                  (list dnd--regexp-1d2+3
+		       dnd--regexp-1d2-3
                        dnd--regexp-1d2
                        dnd--regexp-d2
                        dnd--regexp-+3
+                       dnd--regexp--3
                        dnd--regexp-33%
                        dnd--regexp-33-percent)
                  "\\|"))
@@ -120,9 +123,11 @@ buttons that will print the result of the dice roll depicted."
 	'identity
 	(list
 	 (dnd--match-1d2+3 end)
+	 (dnd--match-1d2-3 end)
 	 (dnd--match-1d2 end)
 	 (dnd--match-d2 end)
 	 (dnd--match-+3 end)
+	 (dnd--match--3 end)
 	 (dnd--match-% end)))))))
 
 (defun dnd--match-1d2+3 (end)
@@ -131,23 +136,37 @@ buttons that will print the result of the dice roll depicted."
 		       (dnd--match-number 2)
 		       (dnd--match-number 3)))
 
-(defun dnd--match-1d2 (end)
+(defun dnd--match-1d2-3 (end)
   (dnd--match-btn-data end
 		       (dnd--match-number 4)
 		       (dnd--match-number 5)
+		       (let ((b (dnd--match-number 6)))
+			 (if b (* -1 b) b))))
+
+(defun dnd--match-1d2 (end)
+  (dnd--match-btn-data end
+		       (dnd--match-number 7)
+		       (dnd--match-number 8)
 		       0))
 
 (defun dnd--match-d2 (end)
   (dnd--match-btn-data end
 		       1
-		       (dnd--match-number 6)
+		       (dnd--match-number 9)
 		       0))
 
 (defun dnd--match-+3 (end)
   (dnd--match-btn-data end
 		       1
 		       20
-		       (dnd--match-number 7)))
+		       (dnd--match-number 10)))
+
+(defun dnd--match--3 (end)
+  (dnd--match-btn-data end
+		       1
+		       20
+		       (let ((b (dnd--match-number 11)))
+			 (if b (* -1 b) b))))
 
 (defun dnd--match-% (end)
   (dnd--match-btn-data end
